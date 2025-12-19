9ccf35ee476f5c43fb4a18953697b612454a1a97
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-12-19 23:02:17 +0500

    Fix: Uncomment RealWorldApplicationsSection in HomePage to display card images

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -19,7 +19,7 @@ const HomePage = () => {
       <Header />
       <FeaturesSection />
       <CoreTopicsSection />
-      // <RealWorldApplicationsSection />
+      <RealWorldApplicationsSection />
       <TestimonialsSection />
     </Layout>
   );
